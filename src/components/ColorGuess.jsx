3188b3b523c68fbb18644e07fc07d6fa16809701
Damog83 <damiengittings@hotmail.co.uk>
2022-10-03 00:33:52 +0100

    remove setAnswer from selection function

--- src/components/ColorGuess.jsx
+++ src/components/ColorGuess.jsx
@@ -1,10 +1,9 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
-import shuffle from '../utils';
+import shuffle from '../shuffle';
 import { useCallback } from 'react';
 
 export default function ColorGuess() {
-
 	const [color, setColor] = useState('');
 	const [choices, setChoices] = useState([]);
 	const [answer, setAnswer] = useState(true);
@@ -19,45 +18,47 @@ export default function ColorGuess() {
 		return hex;
 	};
 
-    const resetGame = useCallback(() => {
-        setAnswer(undefined)
+	const resetGame = useCallback(() => {
+		setAnswer(true);
 		const correctColor = generateColor();
 		setColor(correctColor);
 		setChoices(shuffle([correctColor, generateColor(), generateColor()]));
-    }, [])
-
+	}, []);
 
 	const selection = (choice) => {
 		if (choice === color) {
-			setAnswer(true);
-            resetGame()
+			resetGame();
 		} else {
 			setAnswer(false);
 		}
 	};
 
 	useEffect(() => {
-        resetGame()
+		resetGame();
 	}, [resetGame]);
 
 	return (
 		<>
 			<div>
 				<div className="guess" style={{ background: color }}></div>
-                <div className = 'buttonDiv'>
-				{choices.map((choice) => (
-					<button
-						className="button"
-						key={choice}
-						onClick={() => {
-							selection(choice);
-						}}
-					>
-						{choice}
-					</button>
-				))}
-                </div>
-				{answer === false ? <p className = 'tryAgain'>Try again...</p> : <p className = 'takeAGuess'>Take a guess</p>}
+				<div className="buttonDiv">
+					{choices.map((choice) => (
+						<button
+							className="button"
+							key={choice}
+							onClick={() => {
+								selection(choice);
+							}}
+						>
+							{choice}
+						</button>
+					))}
+				</div>
+				{answer === false ? (
+					<p className="tryAgain">Try again...</p>
+				) : (
+					<p className="takeAGuess">Take a guess</p>
+				)}
 			</div>
 		</>
 	);
